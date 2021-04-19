58d22b6e99f51486dd6bda9fff4959c483c7497b
peleg525 <73443096+peleg525@users.noreply.github.com>
2021-04-19 16:30:00 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ name = 'Ourlogger',         # How you named your package folder (MyLib)
   packages = ['Ourlogger'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'Creat logger object in order to push him to logs table - d_digital_data.models_logs',   # Give a short description about your library
+  description = 'Creat logger object in order to push him to logs table - digital_data.models_logs',   # Give a short description about your library
   author = 'Peleg Wurzel',                   # Type in your name
   author_email = 'Peleg525@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/peleg525/Ourlogger.git',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/peleg525/Ourlogger/archive/refs/tags/V_04.tar.gz',    # I explain this later on
+  url = 'https://github.com/peleg525/Ourlogger_p.git',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/peleg525/Ourlogger_p/archive/refs/tags/V_01.tar.gz',    # I explain this later on
   keywords = ['logs'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'pandas',
